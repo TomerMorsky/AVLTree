a4dc5f54beccd34c3e84a15adba81cba07d5949f
Tomer <tomer@totah.com>
2021-09-16 17:54:54 +0300

    Add tree and make it gengeric

---
+++ AVLTree/AVLTree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVLTree
+{
+    public class Node<T> where T : IComparable<T>
+    {
+        public T data;
+        public Node<T> left;
+        public Node<T> right;
+        public Node(T data)
+        {
+            this.data = data;
+        }
+    }
+
+/*  for the compareto
+      a.compareTo(b) < 0 // a < b
+
+      a.compareTo(b) > 0 // a > b
+
+      a.compareTo(b) == 0 // a == b
+*/
+}
