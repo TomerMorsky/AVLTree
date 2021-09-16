5bb56684009189883a3567dd114751631ec80461
Tomer <tomer@totah.com>
2021-09-16 17:54:54 +0300

    Add tree and make it gengeric

---
+++ AVLTree/AVLTree/AVL.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AVLTree
+{
+    public class AVL<T> where T : IComparable<T>
+    {
+        Node<T> root;
+        public AVL()
+        {
+        }
+        public void Add(T data)
+        {
+            Node<T> newItem = new Node<T>(data);
+            if (root == null)
+            {
+                root = newItem;
+            }
+            else
+            {
+                root = RecursiveInsert(root, newItem);
+            }
+        }
+        private Node<T> RecursiveInsert(Node<T> current, Node<T> n)
+        {
+            if (current == null)
+            {
+                current = n;
+                return current;
+            }
+            else if (n.data < current.data)
+            {
+                current.left = RecursiveInsert(current.left, n);
+                current = balance_tree(current);
+            }
+            else if (n.data > current.data)
+            {
+                current.right = RecursiveInsert(current.right, n);
+                current = balance_tree(current);
+            }
+            return current;
+        }
+        private Node<T> balance_tree(Node<T> current)
+        {
+            int b_factor = balance_factor(current);
+            if (b_factor > 1)
+            {
+                if (balance_factor(current.left) > 0)
+                {
+                    current = RotateLL(current);
+                }
+                else
+                {
+                    current = RotateLR(current);
+                }
+            }
+            else if (b_factor < -1)
+            {
+                if (balance_factor(current.right) > 0)
+                {
+                    current = RotateRL(current);
+                }
+                else
+                {
+                    current = RotateRR(current);
+                }
+            }
+            return current;
+        }
+        public void Delete(T target)
+        {//and here
+            root = Delete(root, target);
+        }
+        private Node<T> Delete(Node<T> current, T target)
+        {
+            Node<T> parent;
+            if (current == null)
+            { return null; }
+            else
+            {
+                //left subtree
+                if (target.CompareTo(current.data) < 0)
+                {
+                    current.left = Delete(current.left, target);
+                    if (balance_factor(current) == -2)//here
+                    {
+                        if (balance_factor(current.right) <= 0)
+                        {
+                            current = RotateRR(current);
+                        }
+                        else
+                        {
+                            current = RotateRL(current);
+                        }
+                    }
+                }
+                //right subtree
+                else if (target.CompareTo(current.data) > 0)
+                {
+                    current.right = Delete(current.right, target);
+                    if (balance_factor(current) == 2)
+                    {
+                        if (balance_factor(current.left) >= 0)
+                        {
+                            current = RotateLL(current);
+                        }
+                        else
+                        {
+                            current = RotateLR(current);
+                        }
+                    }
+                }
+                //if target is found
+                else
+                {
+                    if (current.right != null)
+                    {
+                        //delete its inorder successor
+                        parent = current.right;
+                        while (parent.left != null)
+                        {
+                            parent = parent.left;
+                        }
+                        current.data = parent.data;
+                        current.right = Delete(current.right, parent.data);
+                        if (balance_factor(current) == 2)//rebalancing
+                        {
+                            if (balance_factor(current.left) >= 0)
+                            {
+                                current = RotateLL(current);
+                            }
+                            else { current = RotateLR(current); }
+                        }
+                    }
+                    else
+                    {   //if current.left != null
+                        return current.left;
+                    }
+                }
+            }
+            return current;
+        }
+        public void Find(T key)
+        {
+            if (Find(key, root).data.Equals(key))
+            {
+                Console.WriteLine("{0} was found!", key);
+            }
+            else
+            {
+                Console.WriteLine("Nothing found!");
+            }
+        }
+        private Node<T> Find(T target, Node<T> current)
+        {
+
+            if (target.CompareTo(current.data) < 0)// targer.compareto(current.date) < 0
+            {
+                if (target.Equals(current.data))
+                {
+                    return current;
+                }
+                else
+                    return Find(target, current.left);
+            }
+            else
+            {
+                if (target.Equals(current.data))
+                {
+                    return current;
+                }
+                else
+                    return Find(target, current.right);
+            }
+
+        }
+        public void DisplayTree()
+        {
+            if (root == null)
+            {
+                Console.WriteLine("Tree is empty");
+                return;
+            }
+            InOrderDisplayTree(root);
+            Console.WriteLine();
+        }
+        private void InOrderDisplayTree(Node<T> current)
+        {
+            if (current != null)
+            {
+                InOrderDisplayTree(current.left);
+                Console.Write("({0}) ", current.data);
+                InOrderDisplayTree(current.right);
+            }
+        }
+        private int max(int l, int r)
+        {
+            return l > r ? l : r;
+        }
+        private int getHeight(Node<T> current)
+        {
+            int height = 0;
+            if (current != null)
+            {
+                int l = getHeight(current.left);
+                int r = getHeight(current.right);
+                int m = max(l, r);
+                height = m + 1;
+            }
+            return height;
+        }
+        private int balance_factor(Node<T> current)
+        {
+            int l = getHeight(current.left);
+            int r = getHeight(current.right);
+            int b_factor = l - r;
+            return b_factor;
+        }
+        private Node<T> RotateRR(Node<T> parent)
+        {
+            Node<T> pivot = parent.right;
+            parent.right = pivot.left;
+            pivot.left = parent;
+            return pivot;
+        }
+        private Node<T> RotateLL(Node<T> parent)
+        {
+            Node<T> pivot = parent.left;
+            parent.left = pivot.right;
+            pivot.right = parent;
+            return pivot;
+        }
+        private Node<T> RotateLR(Node<T> parent)
+        {
+            Node<T> pivot = parent.left;
+            parent.left = RotateRR(pivot);
+            return RotateLL(parent);
+        }
+        private Node<T> RotateRL(Node<T> parent)
+        {
+            Node<T> pivot = parent.right;
+            parent.right = RotateLL(pivot);
+            return RotateRR(parent);
+        }
+    }
+}
--- AVLTree/AVLTree/Point.cs
+++ AVLTree/AVLTree/Point.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace AVLTree
 {
-    class Point
+    public class Point : IComparable
     {
         public double X { get; set; }
         public double Y { get; set; }
@@ -14,4 +14,29 @@ namespace AVLTree
             X = x;
             Y = y;
         }
+
+        //Compare just the X field!!
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Point otherPoint = obj as Point;
+            if (otherPoint != null)
+                return this.X.CompareTo(otherPoint.X);
+            else
+                throw new ArgumentException("Object is not a Temperature");
+        }
+
+        //check only the X field!!
+        public override bool Equals(object obj)
+        {
+            return obj is Point point &&
+                   X == point.X;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
+}
