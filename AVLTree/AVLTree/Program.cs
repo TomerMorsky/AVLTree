1561d69ace5931e409767c701db77f9331495c61
Tomer <tomer@totah.com>
2021-09-16 18:03:41 +0300

    tree is working

--- AVLTree/AVLTree/Program.cs
+++ AVLTree/AVLTree/Program.cs
@@ -6,6 +6,13 @@ namespace AVLTree
     {
         static void Main(string[] args)
         {
+            AVL<int> tree = new AVL<int>();
+            tree.Add(5);
+            tree.Add(3);
+            tree.Add(7);
+            tree.Add(2);
+        
+            tree.DisplayTree();
             Console.WriteLine("Hello World!");
         }
     }
