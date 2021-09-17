3126aee7875e627460521e187cb146e504acab6b
Tomer <tomer@totah.com>
2021-09-17 13:09:47 +0300

    Add whatsApp things

--- AVLTree/AVLTree/NearestRightPoint.cs
+++ AVLTree/AVLTree/NearestRightPoint.cs
@@ -6,12 +6,16 @@ namespace AVLTree
 {
     public static class NearestRightPoint
     {
-        private static double min_diff;
+        private static double minDiff;
         private static Point closestPoint;
 
         public static Point GetNearestRightPoint(Node<Point> ptr, double k)
         {
+            minDiff = 999999999;
+            closestPoint = new Point(0, 0);
+
             MaxDiffUtil(ptr, k);
+
             if (closestPoint.X == k)
                 return new Point(0, 0); // Ex2 says if its on the line - return (0,0)
 
@@ -34,9 +38,9 @@ namespace AVLTree
 
             // update min_diff and min_diff_key by checking 
             // current node value 
-            if (min_diff > Math.Abs(ptr.data.X - k))
+            if (minDiff > Math.Abs(ptr.data.X - k))
             {
-                min_diff = Math.Abs(ptr.data.X - k);
+                minDiff = Math.Abs(ptr.data.X - k);
                 closestPoint = ptr.data;
             }
 
