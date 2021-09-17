c1943109f27080bcc429ac7f9dc992fcee46082d
Tomer <tomer@totah.com>
2021-09-17 11:47:01 +0300

    Ex1 Done!

--- AVLTree/AVLTree/Program.cs
+++ AVLTree/AVLTree/Program.cs
@@ -7,14 +7,29 @@ namespace AVLTree
         static void Main(string[] args)
         {
             AVL<Point> tree = new AVL<Point>();
+            var ramdomPoints = GetRandomPointsArray(0, 100, 20);
 
-            tree.Add(new Point(5,0));
-            tree.Add(new Point(3, 0));
-            tree.Add(new Point(7, 0));
-            tree.Add(new Point(2, 0));
-        
+            // Will get into the tree by the X cordinate
+            // (Point is an IComparable and it compared to other points by its X)
+            for (int i = 0; i < ramdomPoints.Length; i++)
+                tree.Add(ramdomPoints[i]); 
+
             tree.DisplayTree();
-            Console.WriteLine("Hello World!");
+        }
+
+        private static Point[] GetRandomPointsArray(double minimum, double maximum, int amountOfPoints)
+        {
+            Random random = new Random();
+            Point[] randomPoints = new Point[amountOfPoints];
+
+            for (int i = 0; i < amountOfPoints; i++)
+            {
+                var x = random.NextDouble() * (maximum - minimum) + minimum;
+                var y = random.NextDouble() * (maximum - minimum) + minimum;
+                randomPoints[i] = new Point(x, y);
+            }
+
+            return randomPoints;
         }
     }
 }
